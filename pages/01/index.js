0e386ab5b8aa1f7276a22da6f9cfd451554bd735
marchfirst01 <marchfirst3113@gmail.com>
2023-07-31 22:24:55 +0900

    Emotion Practice

---
+++ pages/01/index.js
@@ -0,0 +1,81 @@
+import { ArrowDown, ContentWrapper, Line, MenuTitle, MenuTitleClicked, MenuWrapper, MyTitle, MyWrapper, NavigationIconWrapper, NavigationWrapper, ProfileImg, ProfileName, ProfileWrapper, QuestionContent, QuestionNumber, QuestionWrapper, SearchIcon, SearchWrapper, Wrapper } from "../../styles/01/emotion";
+
+export default function FaqPage() {
+
+  return(
+    <>
+      <Wrapper>
+        <SearchWrapper>
+          <SearchIcon src="img/search.png" />
+        </SearchWrapper>
+        <MyWrapper>
+          <MyTitle>마이</MyTitle>
+          <ProfileWrapper>
+            <ProfileImg src="img/profileImage.png" />
+            <ProfileName>임정아</ProfileName>
+          </ProfileWrapper>
+        </MyWrapper>
+        <MenuWrapper>
+          <MenuTitle>공지사항</MenuTitle>
+          <MenuTitle>이벤트</MenuTitle>
+          <MenuTitleClicked>FAQ</MenuTitleClicked>
+          <MenuTitle>Q&A</MenuTitle>
+        </MenuWrapper>
+        <Line></Line>
+        <ContentWrapper>
+          <QuestionWrapper>
+            <QuestionNumber>Q.01</QuestionNumber>
+            <QuestionContent>리뷰 작성은 어떻게 하나요?</QuestionContent>
+          </QuestionWrapper>
+          <ArrowDown src="img/arrow-down.png" />
+        </ContentWrapper>
+        <ContentWrapper>
+          <QuestionWrapper>
+            <QuestionNumber>Q.02</QuestionNumber>
+            <QuestionContent>리뷰 수정/삭제는 어떻게 하나요?</QuestionContent>
+          </QuestionWrapper>
+          <ArrowDown src="img/arrow-down.png" />
+        </ContentWrapper>
+        <ContentWrapper>
+          <QuestionWrapper>
+            <QuestionNumber>Q.03</QuestionNumber>
+            <QuestionContent>아이디/비밀번호를 잊어버렸어요</QuestionContent>
+          </QuestionWrapper>
+          <ArrowDown src="img/arrow-down.png" />
+        </ContentWrapper>
+        <ContentWrapper>
+          <QuestionWrapper>
+            <QuestionNumber>Q.04</QuestionNumber>
+            <QuestionContent>회원탈퇴를 하고싶어요.</QuestionContent>
+          </QuestionWrapper>
+          <ArrowDown src="img/arrow-down.png" />
+        </ContentWrapper>
+        <ContentWrapper>
+          <QuestionWrapper>
+            <QuestionNumber>Q.05</QuestionNumber>
+            <QuestionContent>출발지 설정은 어떻게 하나요?</QuestionContent>
+          </QuestionWrapper>
+          <ArrowDown src="img/arrow-down.png" />
+        </ContentWrapper>
+        <ContentWrapper>
+          <QuestionWrapper>
+            <QuestionNumber>Q.06</QuestionNumber>
+            <QuestionContent>비밀번호를 변경하고 싶어요.</QuestionContent>
+          </QuestionWrapper>
+          <ArrowDown src="img/arrow-down.png" />
+        </ContentWrapper>
+        <Line></Line>
+        <NavigationWrapper>
+          <NavigationIconWrapper><img src="img/home.png"/>홈
+          </NavigationIconWrapper>
+          <NavigationIconWrapper><img src="img/map.png"/>잇츠로드
+          </NavigationIconWrapper>
+          <NavigationIconWrapper><img src="img/heart.png"/>마이찜
+          </NavigationIconWrapper>
+          <NavigationIconWrapper><img src="img/myprofile.png"/>마이
+          </NavigationIconWrapper>
+        </NavigationWrapper>
+      </Wrapper>
+    </>
+  )
+}
---
+++ styles/01/emotion.js
@@ -0,0 +1,134 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  width: 640px;
+  height: 1095px;
+  padding: 50px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+export const SearchWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+`
+
+export const SearchIcon = styled.img`
+  width: 23px;
+  height: 23px;
+`
+
+export const MyWrapper = styled.div`
+  width: 100%;
+
+  margin-bottom: 30px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const MyTitle = styled.div`
+  font-size: 40px;
+  font-weight: 700px;
+`
+
+export const ProfileWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+export const ProfileImg = styled.img`
+  width: 60px;
+  height: 60px;
+  
+  margin-right: 5px;
+`
+
+export const ProfileName = styled.div`
+  font-size: 24px;
+  font-weight: 700px;
+`
+
+export const MenuWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+export const MenuTitle = styled.div`
+  font-size: 30px;
+  font-weight: 700px;
+  color: #CACACA
+`
+
+export const MenuTitleClicked = styled.div`
+  font-size: 30px;
+  font-weight: 700px;
+  color: #FF1B6D
+`
+
+export const Line = styled.div`
+  width: 100%;
+  height: 1px;
+
+  margin-top: 50px;
+  margin-bottom: 50px;
+
+  border-top: 1px solid #CACACA;
+`
+
+export const ContentWrapper = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+export const QuestionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  margin-bottom: 30px;
+`
+
+export const QuestionNumber = styled.div`
+  font-size: 14px;
+  color: #ADADAD;
+  margin-bottom: 5px;
+`
+
+export const QuestionContent = styled.div`
+  font-size: 24px;
+  font-weight: 400px;
+`
+
+export const ArrowDown = styled.img`
+  width: 23px;
+  height: 23px;
+`
+
+export const NavigationWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const NavigationIconWrapper = styled.div`
+  width: 70px;
+  height: 50px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
